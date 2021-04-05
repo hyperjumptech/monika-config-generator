a7749eadb5eea87c860d885ce8d537270f213018
muslim <muslim.ilmiawan@gmail.com>
2021-04-05 08:55:39 +0700

    fix(download): fix code formatting for download page

--- pages/download.tsx
+++ pages/download.tsx
@@ -26,9 +26,7 @@ export default function Download(): JSX.Element {
             </p>
             <div className="mt-6 py-3">
               <Button type="submit" onClick={handleDownload}>
-              <div className="py-3 px-3">
-                Download Config File
-                </div>
+                <div className="py-3 px-3">Download Config File</div>
               </Button>
             </div>
             <div className="mt-12 py-3">
@@ -36,26 +34,47 @@ export default function Download(): JSX.Element {
               <p className="mt-2 text-sm sm:text-lg">
                 <ul className="">
                   <li>
-                    Use the configuration file with Monika. Please refer to <a
-                    href="https://hyperjumptech.github.io/monika/overview"
-                    target="_blank"
-                    rel="noopener noreferrer" className="underlined">our documentation</a> for more information.
+                    Use the configuration file with Monika. Please refer to{' '}
+                    <a
+                      href="https://hyperjumptech.github.io/monika/overview"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="underlined">
+                      our documentation
+                    </a>{' '}
+                    for more information.
                   </li>
                   <li>
                     You can also customize the configuration even further by
-                    clicking <a
-                    href="https://hyperjumptech.github.io/monika/overview"
-                    target="_blank"
-                    rel="noopener noreferrer" className="underlined">here</a>.
+                    clicking{' '}
+                    <a
+                      href="https://hyperjumptech.github.io/monika/overview"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="underlined">
+                      here
+                    </a>
+                    .
                   </li>
-                  <li><a
-                    href="https://hyperjumptech.github.io/monika/overview"
-                    target="_blank"
-                    rel="noopener noreferrer" className="underlined">Tell your friends</a> about Monika.</li>
-                  <li><a
-                    href="https://github.com/hyperjumptech/monika/issues"
-                    target="_blank"
-                    rel="noopener noreferrer" className="underlined">Report a bug or give us some feedback!</a></li>
+                  <li>
+                    <a
+                      href="https://hyperjumptech.github.io/monika/overview"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="underlined">
+                      Tell your friends
+                    </a>{' '}
+                    about Monika.
+                  </li>
+                  <li>
+                    <a
+                      href="https://github.com/hyperjumptech/monika/issues"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="underlined">
+                      Report a bug or give us some feedback!
+                    </a>
+                  </li>
                 </ul>
               </p>
             </div>
