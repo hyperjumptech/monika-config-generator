f41d26ee571fa9128d5240952175f85ad22ee773
Denny Pradipta <denny@hyperjump.tech>
2021-04-07 14:32:53 +0700

    feat: created components for reusable

---
+++ components/textarea/index.tsx
@@ -0,0 +1,27 @@
+import { TextareaHTMLAttributes } from 'react';
+
+interface TextareaProps
+  extends Omit<TextareaHTMLAttributes<HTMLTextAreaElement>, 'className'> {
+  label?: string;
+}
+
+export default function Textarea({
+  label,
+  ...textAreaProps
+}: TextareaProps): JSX.Element {
+  return (
+    <>
+      {label && (
+        <label
+          htmlFor={textAreaProps.id}
+          className="text-gray-400 text-sm sm:text-lg">
+          {label}
+        </label>
+      )}
+      <textarea
+        {...textAreaProps}
+        className="w-full border bg-white border-gray-400 rounded-md shadow-md py-3 px-5 text-sm sm:text-lg placeholder-gray-400 h-full"
+      />
+    </>
+  );
+}
