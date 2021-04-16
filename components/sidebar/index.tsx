08382fa051226c0fd500bd35a1b35ff13d8fe354
Denny Pradipta <denny@hyperjump.tech>
2021-04-16 11:44:13 +0700

    refactor: modularize sidebar

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -1,49 +1,33 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent } from 'react';
 
 type Menu = 'Probe' | 'Notification';
 
-interface SidebarChildrenProps {
+interface SidebarProps {
   activeMenu: Menu;
-  setActiveMenu: (menu: Menu) => void;
-}
-
-interface SidebarComponent {
-  children: (props: SidebarChildrenProps) => React.ReactNode;
-}
-
-const Sidebar: FunctionComponent<SidebarComponent> = ({ children }) => {
-  const [activeMenu, setActiveMenu] = useState<Menu>('Probe');
-
-  const handleSetActiveMenu = (
+  onMenuChange: (
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
     menu: Menu
-  ) => {
-    event.preventDefault();
+  ) => void;
+}
 
-    setActiveMenu(menu);
-  };
-
+const Sidebar: FunctionComponent<SidebarProps> = ({
+  activeMenu,
+  onMenuChange,
+}) => {
   return (
-    <div className="flex flex-col md:flex-row space-x-0 space-y-8 md:space-y-0 md:space-x-8">
-      <div className="w-full md:w-3/12">
-        <ul>
-          {(['Probe', 'Notification'] as Menu[]).map((menu) => (
-            <li
-              key={menu}
-              className={`border border-solid p-4 first:rounded-t-md last:rounded-b-md border-l-8 ${
-                activeMenu === menu && 'border-gray-600'
-              }`}>
-              <a href="#" onClick={(event) => handleSetActiveMenu(event, menu)}>
-                {menu}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="w-full md:w-9/12">
-        {children && children({ activeMenu, setActiveMenu })}
-      </div>
-    </div>
+    <ul>
+      {(['Probe', 'Notification'] as Menu[]).map((menu) => (
+        <li
+          key={menu}
+          className={`border border-solid p-4 first:rounded-t-md last:rounded-b-md border-l-8 ${
+            activeMenu === menu && 'border-gray-600'
+          }`}>
+          <a href="#" onClick={(event) => onMenuChange(event, menu)}>
+            {menu}
+          </a>
+        </li>
+      ))}
+    </ul>
   );
 };
 
--- pages/advanced.tsx
+++ pages/advanced.tsx
@@ -16,34 +16,49 @@ export default function Advanced(): JSX.Element {
   const { probeData, handleAddProbe } = useContext(ProbeContext);
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
 
-  const renderProbe = () => {
-    return (
-      <div>
-        {probeData.map((probe: Probe) => (
-          <ProbeCard probe={probe} key={probe.id} id={probe.id} />
-        ))}
-        <button
-          onClick={handleAddProbe}
-          className="w-full border-4 border-dashed rounded-md p-4">
-          <p>Add another probe</p>
-        </button>
-      </div>
-    );
+  type Menu = 'Probe' | 'Notification';
+  const [activeMenu, setActiveMenu] = useState<Menu>('Probe');
+  const handleSetActiveMenu = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    menu: Menu
+  ) => {
+    event.preventDefault();
+
+    setActiveMenu(menu);
   };
 
-  const renderNotification = () => {
-    return (
-      <div>
-        {notificationData.map((notif: Notification) => (
-          <NotifCard key={notif.id} id={notif.id} type={notif.type} />
-        ))}
-        <button
-          onClick={handleAddNotification}
-          className="w-full border-4 border-dashed rounded-md p-4">
-          <p>Add another notification</p>
-        </button>
-      </div>
-    );
+  const render = () => {
+    if (activeMenu === 'Notification') {
+      return (
+        <div>
+          {notificationData.map((notif: Notification) => (
+            <NotifCard key={notif.id} id={notif.id} type={notif.type} />
+          ))}
+          <button
+            onClick={handleAddNotification}
+            className="w-full border-4 border-dashed rounded-md p-4">
+            <p>Add another notification</p>
+          </button>
+        </div>
+      );
+    }
+
+    if (activeMenu === 'Probe') {
+      return (
+        <div>
+          {probeData.map((probe: Probe) => (
+            <ProbeCard probe={probe} key={probe.id} id={probe.id} />
+          ))}
+          <button
+            onClick={handleAddProbe}
+            className="w-full border-4 border-dashed rounded-md p-4">
+            <p>Add another probe</p>
+          </button>
+        </div>
+      );
+    }
+
+    return <>Nothing to render</>;
   };
 
   return (
@@ -57,11 +72,15 @@ export default function Advanced(): JSX.Element {
           onClose={() => setIsModalVisible(false)}
         />
       </div>
-      <Sidebar>
-        {({ activeMenu }) =>
-          activeMenu === 'Probe' ? renderProbe() : renderNotification()
-        }
-      </Sidebar>
+      <div className="flex flex-col md:flex-row space-x-0 space-y-8 md:space-y-0 md:space-x-8">
+        <div className="w-full md:w-3/12">
+          <Sidebar
+            activeMenu={activeMenu}
+            onMenuChange={(event, menu) => handleSetActiveMenu(event, menu)}
+          />
+        </div>
+        <div className="w-full md:w-9/12">{render()}</div>
+      </div>
     </Layout>
   );
 }
