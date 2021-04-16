290b160b14a57e34d38d6580c287687fb152c214
Armedi <armedi@outlook.com>
2021-04-16 08:15:35 +0700

    feat: remove next button on /what-do-you-want page

--- pages/what-do-you-want.tsx
+++ pages/what-do-you-want.tsx
@@ -1,38 +1,13 @@
-import { SyntheticEvent, useState } from 'react';
 import { Layout, Radio, Button } from '../components';
 import { useRouter } from 'next/router';
-
-const WEB_PAGE = 'web_page';
-const WEB_FORM = 'web_form';
-const API_SERVER = 'api_server';
 
 export default function WhatDoYouWant(): JSX.Element {
   const router = useRouter();
 
-  const [nextPage, setNextPage] = useState('');
-
-  const handleNext = (e: SyntheticEvent) => {
-    e.preventDefault();
-
-    switch (nextPage) {
-      case WEB_PAGE:
-        router.push('/web-page');
-        break;
-      case WEB_FORM:
-        router.push('/web-form');
-        break;
-      case API_SERVER:
-        router.push('/api-entry');
-        break;
-      default:
-        break;
-    }
-  };
-
   return (
     <Layout>
       <div className="lg:py-20 xl:py-32 xl:px-80">
-        <form onSubmit={handleNext}>
+        <form>
           <fieldset>
             <div>
               <legend className="text-base sm:text-lg font-medium">
@@ -42,22 +17,22 @@ export default function WhatDoYouWant(): JSX.Element {
             <div className="mt-12 space-y-4">
               <Radio
                 name="what"
-                onClick={setNextPage}
-                value={WEB_PAGE}
+                onClick={() => router.push('/web-page')}
+                value="web_page"
                 help="Select this if you want to regularly monitor a website, e.g., https://github.com">
                 A web page
               </Radio>
               <Radio
                 name="what"
-                onClick={setNextPage}
-                value={WEB_FORM}
+                onClick={() => router.push('/web-form')}
+                value="web_form"
                 help="Select this if you want to be notified when a form submission to your website fails.">
                 A web form
               </Radio>
               <Radio
                 name="what"
-                onClick={setNextPage}
-                value={API_SERVER}
+                onClick={() => router.push('/api-entry')}
+                value="api_server"
                 help="Select this if you want to monitor the end points of your API server.">
                 API server
               </Radio>
@@ -67,7 +42,6 @@ export default function WhatDoYouWant(): JSX.Element {
             <Button type="button" outline onClick={() => router.back()}>
               Back
             </Button>
-            <Button type="submit">Next</Button>
           </div>
         </form>
       </div>
