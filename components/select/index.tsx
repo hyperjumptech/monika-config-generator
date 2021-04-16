9bc90e130a27a99b0b12a3341cbc1ebb6111f2a3
Denny Pradipta <denny@hyperjump.tech>
2021-04-16 17:51:02 +0700

    fix: arrow margin (#48)

--- components/select/index.tsx
+++ components/select/index.tsx
@@ -18,19 +18,27 @@ export default function Select({
   ...selectProps
 }: SelectProps): JSX.Element {
   return (
-    <div className="space-y-3">
+    <div>
       {label && (
-        <label
-          htmlFor={selectProps.id}
-          className="text-gray-400 text-sm sm:text-lg">
+        <label htmlFor={selectProps.id} className="text-sm sm:text-lg">
           {label}
         </label>
       )}
-      <select
-        {...selectProps}
-        className="w-full border bg-white border-gray-400 rounded-md shadow-md py-3 px-5 text-sm sm:text-lg placeholder-gray-400 h-full">
-        {children}
-      </select>
+      <div className="relative inline-block w-full text-gray-700">
+        <select
+          {...selectProps}
+          className="w-full border appearance-none bg-white border-gray-400 rounded-md shadow-md py-3 px-5 pr-8 text-sm sm:text-lg placeholder-gray-400 h-full">
+          {children}
+        </select>
+        <div className="absolute inset-y-0 right-0 mr-1 flex items-center px-2 pointer-events-none">
+          <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20">
+            <path
+              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+              clipRule="evenodd"
+              fillRule="evenodd"></path>
+          </svg>
+        </div>
+      </div>
     </div>
   );
 }
