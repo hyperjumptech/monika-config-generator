ff52c4a46aaf2bc805ab010f7b639ca7fdf1cfe5
herenacreative <sintaherena@gmail.com>
2022-07-29 15:24:29 +0700

    fix prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -127,14 +127,20 @@ export default function Home(): JSX.Element {
         <div className="card absolute flex md:left-36 left-0 md:right-36 right-0 md:rounded-xl rounded-none lg:p-10 p-6 px-4 xl:mt-5 mt-3">
           <p className="text-white font-medium lg:text-xl text-center">
             If you use{' '}
-            <a href="https://code.visualstudio.com/" className="font-bold">Visual Studio Code</a> on a
-            daily basis, you can write Monika configuration easily with the help
-            of auto completion and validation by installing the{' '}
-            <a href="https://marketplace.visualstudio.com/items?itemName=redhat.vscode-yaml" className="font-bold">
+            <a href="https://code.visualstudio.com/" className="font-bold">
+              Visual Studio Code
+            </a>{' '}
+            on a daily basis, you can write Monika configuration easily with the
+            help of auto completion and validation by installing the{' '}
+            <a
+              href="https://marketplace.visualstudio.com/items?itemName=redhat.vscode-yaml"
+              className="font-bold">
               YAML extension
             </a>
             . More information can be found in our{' '}
-            <a href="https://medium.com/hyperjump-tech/creating-monika-configuration-from-scratch-using-autocomplete-in-visual-studio-code-d7bc86c1d36a" className="font-bold">
+            <a
+              href="https://medium.com/hyperjump-tech/creating-monika-configuration-from-scratch-using-autocomplete-in-visual-studio-code-d7bc86c1d36a"
+              className="font-bold">
               Creating Monika Configuration from Scratch using Autocomplete in
               Visual Studio Code
             </a>{' '}
