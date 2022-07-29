be957bc4bb201e4a5c62888db20746e43bde6f13
herenacreative <sintaherena@gmail.com>
2022-07-29 10:58:05 +0700

    Add information about using vscode and YAML extension (#112)

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,7 +51,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout>
       <HeadPage description="Web app to generate configuration file for Monika, the open source and free monitoring tool." />
-      <div className="lg:py-20 xl:py-32 xl:px-80">
+      <div className="lg:py-20 xl:py-32 xl:px-80 md:mb-0 mb-12">
         <form>
           <fieldset>
             <div>
@@ -124,7 +124,33 @@ export default function Home(): JSX.Element {
             />
           </div>
         </form>
+        <div className="card absolute flex md:left-36 left-0 md:right-36 right-0 md:rounded-xl rounded-none lg:p-10 p-6 px-4 xl:mt-5 mt-3">
+          <p className="text-white font-medium lg:text-xl text-center">
+            If you use{' '}
+            <a href="https://code.visualstudio.com/" className="font-bold">Visual Studio Code</a> on a
+            daily basis, you can write Monika configuration easily with the help
+            of auto completion and validation by installing the{' '}
+            <a href="https://marketplace.visualstudio.com/items?itemName=redhat.vscode-yaml" className="font-bold">
+              YAML extension
+            </a>
+            . More information can be found in our{' '}
+            <a href="https://medium.com/hyperjump-tech/creating-monika-configuration-from-scratch-using-autocomplete-in-visual-studio-code-d7bc86c1d36a" className="font-bold">
+              Creating Monika Configuration from Scratch using Autocomplete in
+              Visual Studio Code
+            </a>{' '}
+            blog post.
+          </p>
+        </div>
       </div>
+      <style jsx>
+        {`
+          .card {
+            background: linear-gradient(350deg, #2fdcdc, rgba(255, 0, 0, 0) 20%),
+              linear-gradient(45deg, #2fdcdc, rgba(0, 255, 0, 0) 30.71%),
+              linear-gradient(336deg, #987ce8, #987ce8 80.71%);
+          }
+        `}
+      </style>
     </Layout>
   );
 }
